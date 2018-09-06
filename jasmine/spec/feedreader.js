546ef0591d5f8800edfc309016a37b392c4d8eab
Sabrina Tolle <sabrinatolle@outlook.com>
2018-09-06 08:19:29 -0400

    test after loadFeed function

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -113,14 +113,15 @@ $(function() {
         beforeEach(function(done) {
             loadFeed(0, done);
         });
+        
+        
+        
+        it('should loadFeed and render the entry and .feed container', function () {
+            expect($('.feed').has('.entry').length).not.toBe(0);
+        });
+     });
 
-    it('completes work', function() {
-        const feed = document.querySelector('.feed');
-        expect(feed.children.length > 0).toBe(true);
-    });
-
-    });
-
+    
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe("New Feed Selection", function() {
 
